13ceb832deae8f2917debb742b83fe870fec699a
leizleho <ho.leizle@gmail.com>
2019-04-29 20:46:03 -0700

    update index page with graphql server

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import routes from './src/routes/crmRoutes';
+import graphqlHTTP from 'express-graphql';
 
 const app = express();
-const PORT = 3000;
-
-// mongoose connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb', {
-    useMongoClient: true
-});
-
-// bodyparser setup
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-routes(app);
+const PORT = 8080;
 
 // serving static files
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
+app.get('/', (req, res) => res.send(`GraphQL is running`));
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
 );
 
 app.listen(PORT, () =>
-    console.log(`your server is running on port ${PORT}`)
+  console.log(`your server is running on localhost:${PORT}/graphql`)
 );
